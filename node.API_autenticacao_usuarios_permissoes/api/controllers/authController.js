c0bc4f0563af2256165631998ce8d8f780a2905f
Gustavo Gomes Pucienik <gomespucienik@gmail.com>
2023-11-17 20:20:38 -0300

    terminando o crud

--- node.API_autenticacao_usuarios_permissoes/api/controllers/authController.js
+++ node.API_autenticacao_usuarios_permissoes/api/controllers/authController.js
@@ -2,14 +2,14 @@ const AuthService = require("../services/authService.js");
 
 const authService = new AuthService()
 
-class AuthController{
-  static async login(req, res){
+class AuthController {
+  static async login(req, res) {
     const { email, senha } = req.body;
     try {
-      const login = await authService.login({email, senha});
+      const login = await authService.login({ email, senha });
       return res.status(200).send(login);
     } catch (error) {
-      res.status(400).send({message: error.message})
+      res.status(400).send({ message: error.message })
     }
   }
 }
