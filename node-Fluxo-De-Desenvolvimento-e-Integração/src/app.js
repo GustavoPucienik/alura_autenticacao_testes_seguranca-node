025efaaa6dfb712b47b59a77b0a6db95fcf109b7
GustavoPucienik <gomespucienik@gmail.com>
2023-12-18 22:24:15 -0300

    feat: :sparkles: add:unleash no projeto

--- node-Fluxo-De-Desenvolvimento-e-Integração/src/app.js
+++ node-Fluxo-De-Desenvolvimento-e-Integração/src/app.js
@@ -1,8 +1,18 @@
+/* eslint-disable linebreak-style */
 import express from 'express';
 import routes from './routes/index.js';
+import unleash from './services/unleash.js';
 
 const app = express();
 app.use(express.json());
 routes(app);
+
+setInterval(() => {
+  if (unleash.isEnabled('eventos')) {
+    console.log('eventos habilitado');
+  } else {
+    console.log('eventos desabilitado');
+  }
+}, 5000);
 
 export default app;
